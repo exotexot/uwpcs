e79b706ea07b304b415cae32a6a8637a378df91d
vinzenz <vinzenz@nsynk.de>
2020-10-27 17:08:01 +0100

    init

---
+++ windows/uwpcs/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::uwpcs::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::uwpcs::implementation
+
+
