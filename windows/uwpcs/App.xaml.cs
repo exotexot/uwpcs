9ff2da2e197ba339a7dc772d374de47fd7f6b869
vinzenz <vinzenz@nsynk.de>
2020-11-03 17:18:50 +0100

    Working c# base windows repo with c# dependency and enabled .net native compiler

---
+++ windows/uwpcs/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.ReactNative;
+
+namespace uwpcs
+{
+    sealed partial class App : ReactApplication
+    {
+        public App()
+        {
+            MainComponentName = "uwpcs";
+
+#if BUNDLE
+            JavaScriptBundleFile = "index.windows";
+            InstanceSettings.UseWebDebugger = false;
+            InstanceSettings.UseFastRefresh = false;
+#else
+            JavaScriptMainModuleName = "index";
+            InstanceSettings.UseWebDebugger = false;
+            InstanceSettings.UseFastRefresh = true;
+#endif
+
+#if DEBUG
+            InstanceSettings.EnableDeveloperMenu = true;
+#else
+            InstanceSettings.EnableDeveloperMenu = false;
+#endif
+
+            Microsoft.ReactNative.Managed.AutolinkedNativeModules.RegisterAutolinkedNativeModulePackages(PackageProviders); // Includes any autolinked modules
+            
+            PackageProviders.Add(new Microsoft.ReactNative.Managed.ReactPackageProvider()); // Includes any modules in this project
+
+            PackageProviders.Add(new RNDeviceInfo.ReactPackageProvider());
+
+            InitializeComponent();
+        }
+    }
+}
